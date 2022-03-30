fa608ffd56f2eaeba151d921f56caef1fcaa7d25
Alex Lin <alexlin1822@hotmail.com>
2022-03-30 13:58:38 -0400

    Init Frontend

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa44q0g12WlsSyar5d7',
+        issuer: 'https://dev-8576707.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  luv2shopApiUrl: "https://localhost:8443/api",
+  stripePublishableKey: "pk_test_51KghcBD1qZo7HdehBUpYWrABlNSYhXPrEQ8JZiGmk9XtRUInjs6e54k9disCM3HzpHqbwy1FoexREVm7yP3Z5XfD007NSku4wr"
+};
+
+
